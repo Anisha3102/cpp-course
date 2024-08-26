d5809090ee147644a3c5788fdc846d4579bec7bd
Anisha3102 <anisha31102002@gmail.com>
2024-08-26 19:53:00 +0530

    Add files via upload

---
+++ series print/Q4.cpp
@@ -0,0 +1,19 @@
+
+// 4.print 1 2 4 8 32 ....
+    //  a=1 b=2 c= power of 2
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+    int arr[10];
+    
+    
+    for(int i=0;i<10;i++){
+         arr[i] = 1 << i;
+    }
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+}
