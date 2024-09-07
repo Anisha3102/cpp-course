5838a2a8dcdf29ac3ec8dd1596809550d7fd31f6
Anisha3102 <anisha31102002@gmail.com>
2024-09-07 17:30:02 +0530

    Add files via upload

---
+++ Class/Q3.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class ABC{
+public:
+
+int sum(){
+    return c+d;
+}
+int multi(){
+   return c*d; 
+}
+
+
+private:
+int c=10;
+int d=20;
+
+};
+
+int main()
+{
+    ABC obj;
+    
+    cout<<obj.sum()<<endl<<obj.multi();
+    
+    return 0;
+}
