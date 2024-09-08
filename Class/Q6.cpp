87338a3c3374967f558309b2a81a7cf766708249
Anisha3102 <anisha31102002@gmail.com>
2024-09-09 02:51:20 +0530

    Add files via upload

---
+++ Class/Q6.cpp
@@ -0,0 +1,21 @@
+// parameterized constructor
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Abc{
+    public:
+    
+    Abc(string k){
+        cout<<k;
+    }
+};
+
+int main()
+{
+  Abc obj("anisha");
+  
+
+    return 0;
+}
+
