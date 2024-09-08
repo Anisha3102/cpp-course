50b22711bcfa7811ae9eb28b5419a73e1c9ed8b8
Anisha3102 <anisha31102002@gmail.com>
2024-09-08 15:40:26 +0530

    Add files via upload

---
+++ Class/Q5.cpp
@@ -0,0 +1,20 @@
+// default constructor
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Abc{
+    public:
+    Abc(){
+    cout<<"this is a function";
+    }
+};
+
+int main()
+{
+  Abc obj;
+
+    return 0;
+}
+
+
