3628cd92720b3d0b361279d1f227d5f152e91718
Anisha3102 <anisha31102002@gmail.com>
2024-09-25 12:38:52 +0530

    Add files via upload

---
+++ Patterns/Q01.cpp
@@ -0,0 +1,19 @@
+//Q01
+//  *
+//  **
+//  ***
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
