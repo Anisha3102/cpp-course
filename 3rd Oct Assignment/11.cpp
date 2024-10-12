f875e4de0006c88362a706eca4faa6751ccfcdd3
Anisha3102 <anisha31102002@gmail.com>
2024-10-12 23:02:28 +0530

    Add files via upload

---
+++ 3rd Oct Assignment/11.cpp
@@ -0,0 +1,11 @@
+//11 Use the at method to safely access the 7th element of a vector of doubles.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<double>arr{2334,4567,234567,5667,32456,4567,67788,45667};
+    cout<<arr.at(7);
+    
+    return 0;
+}
