f9afc80efa58d164c91efa0e6d890b79018279de
Anisha3102 <anisha31102002@gmail.com>
2024-10-13 22:14:02 +0530

    Add files via upload

---
+++ 8th Oct Assignment/03.cpp
@@ -0,0 +1,22 @@
+//3. print all the odd values
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 4, 6, 2, 5, 3};
+    int n = arr.size();
+
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]%2!=0)
+        {
+           cout<<arr[i]<<" ";
+        }
+    }
+
+    
+}
+
