bcf2fab54369fc9396ac14a8b7e2ee0ef57d0530
Anisha3102 <anisha31102002@gmail.com>
2024-10-15 13:47:14 +0530

    Add files via upload

---
+++ 8th Oct Assignment/07.cpp
@@ -0,0 +1,21 @@
+//7. cal sum of all odd values of an array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 4, 6, 2, 5, 3};
+    int n = arr.size();
+    int sum=0;
+
+
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]%2!=0){
+        sum=sum+arr[i];
+        }
+    }
+
+    cout<<sum;
+}
