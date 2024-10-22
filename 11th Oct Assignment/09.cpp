1e948f1a37726c2a049f091601820b5b7da5e135
Anisha3102 <anisha31102002@gmail.com>
2024-10-22 15:25:57 +0530

    Add files via upload

---
+++ 11th Oct Assignment/09.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "jhasjbasd";
+    cout<< typeid(s).name()<<"\n";
+    int a=27;
+    cout<<typeid(a).name()<<"\n";
+    float b=28;
+    cout<<typeid(b).name()<<"\n";
+    long c =564649897;
+    cout<<typeid(c).name();
+}
