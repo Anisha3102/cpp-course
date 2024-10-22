e58379467b09cd736ba8eff453085c911d3718aa
Anisha3102 <anisha31102002@gmail.com>
2024-10-22 15:24:32 +0530

    Add files via upload

---
+++ 11th Oct Assignment/07.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int s = 45;
+   int p=  95 ;
+   cout<<typeid(s).name()<<"\n";
+   string p1 = to_string(s);
+   string p2 = to_string(p);
+   cout<<p1+p2<<"\n";
+//   cout<<typeid(p).name();
+ 
+}
