e6ede2af4d8affda72199674009cd7c1890682bf
Anisha3102 <anisha31102002@gmail.com>
2024-10-22 15:25:33 +0530

    Add files via upload

---
+++ 11th Oct Assignment/08.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "2700";
+    string p = "7800";
+    int p1 = stoi(s);
+    int q2 = stoi(p);
+    cout<<p1+q2<<" ";
+ 
+ 
+}
