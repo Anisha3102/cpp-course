213c178d608c1e8358a8bcb620dec71fde82ea43
Anisha3102 <anisha31102002@gmail.com>
2024-10-22 15:24:00 +0530

    Add files via upload

---
+++ 11th Oct Assignment/06.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string t = "12:10";
+   string s1 = t.substr(0,2);
+   cout<<s1<<"\n";
+   string s2 = t.substr(3);
+   cout<<s2;
+}
