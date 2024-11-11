648d600d4d233f179d0c03ca1f92cca7d49642ea
Anisha3102 <anisha31102002@gmail.com>
2024-11-11 20:34:57 +0530

    Add files via upload

---
+++ 5th Nov Assignment/03.cpp
@@ -0,0 +1,15 @@
+//  [1,2,3,4,5,6,7,8,9] 
+// a) find total number of elements in the array 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+    int totalElements = arr.size();
+
+    cout << "Total number of elements in the array: " << totalElements << endl;
+
+    return 0;
+}
