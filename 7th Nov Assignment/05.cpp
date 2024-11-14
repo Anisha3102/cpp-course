a57b34b0d637406ee116f8dddb6bbff29a3d46f0
Anisha3102 <anisha31102002@gmail.com>
2024-11-14 12:34:44 +0530

    Add files via upload

---
+++ 7th Nov Assignment/05.cpp
@@ -0,0 +1,29 @@
+//5.Find the index of any specific element from an array.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findIndex(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]); 
+    int target = 30; 
+
+    int index = findIndex(arr, size, target);
+
+    if (index != -1) {
+        cout << "Element " << target << " found at index " << index << endl;
+    } else {
+        cout << "Element " << target << " not found in the array." << endl;
+    }
+
+    return 0;
+}
