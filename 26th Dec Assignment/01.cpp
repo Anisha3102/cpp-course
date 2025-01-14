a008cd5275bab289c887b25fcbf5f362c61cf9f3
Anisha3102 <anisha31102002@gmail.com>
2025-01-14 19:33:34 +0530

    Add files via upload

--- 26th Dec Assignment/01.cpp
+++ 26th Dec Assignment/01.cpp
@@ -1,38 +1,16 @@
-//01 Write a program to check if each element in an array is a prime number or not.
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-bool isPrime(int num) {
-    if (num <= 1) {
-        return false;    }
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return false; 
-        }
-    }
-    return true; 
+void func(int n)
+{
+    if(n==-1)
+        return;
+    cout<<n<<" ";
+    func(n-1);
+    cout<<n<<"\n";
 }
-
-int main() {
-    int n;
-       cout << "Enter the number of elements in the array: ";
-    cin >> n;
-
-    int arr[n];
-    cout << "Enter " << n << " elements of the array: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-       for (int i = 0; i < n; i++) {
-        if (isPrime(arr[i])) {
-            cout << arr[i] << " is a prime number." << endl;
-        } else {
-            cout << arr[i] << " is not a prime number." << endl;
-        }
-    }
-
-    return 0;
+int main()
+{
+    int n=5;
+    func(n);
+   
 }
-
