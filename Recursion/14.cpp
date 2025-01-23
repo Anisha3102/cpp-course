075b6dbeea0b242db2ce347247f87409b3558dfd
Anisha3102 <anisha31102002@gmail.com>
2025-01-23 23:36:36 +0530

    Add files via upload

---
+++ Recursion/14.cpp
@@ -0,0 +1,23 @@
+// Function to print the fibonacci
+// series in reverse order.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void fibo(int n, int a, int b)
+{
+	if (n > 0) {
+
+		fibo(n - 1, b, a + b);
+
+		cout << a << " ";
+	}
+}
+
+int main()
+{
+	int N = 10;
+	fibo(N, 0, 1);
+	return 0;
+}
